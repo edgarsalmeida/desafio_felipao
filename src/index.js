823d928fd604a1d301415258cc2c5647656bc216
edgarsalmeida <edgar.dasilvaalmeida@gmail.com>
2026-01-06 20:19:53 -0300

    meu heori

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
-let nome = "Edgar"
-console.log("Meu nome é " + nome);
+let nome = "Guerreiro Ninja";
+let xp = 9001;
+
+let nivel;
+
+if (xp <= 1000){
+    nivel = "Ferro";
+}else if (xp <= 2000){
+    nivel = "Bronze";
+}else if (xp <= 5000){
+    nivel = "Prata";
+}else if (xp <= 7000){
+    nivel = "Ouro";
+}else if (xp <= 8000){
+    nivel = "Platina";
+}else if (xp <= 9000){
+    nivel = "Ascendente";
+}else if (xp <= 10000){
+    nivel = "Imortal";
+}else{
+    nivel = "Radiante";
+}
+console.log("O Heroi de nome " + nome + " está o nível: " + nivel);
